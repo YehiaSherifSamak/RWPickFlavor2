c4fd843effeb5be9d5c6b76b95681f6cd2401197
Yehia Samak <mostafa.mohsen@softxpert.com>
2021-02-08 13:10:02 +0200

    Initial Commit

---
+++ RWPickFlavor2/RWPickFlavor2.h
@@ -0,0 +1,18 @@
+//
+//  RWPickFlavor2.h
+//  RWPickFlavor2
+//
+//  Created by Soft Xpert on 2/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RWPickFlavor2.
+FOUNDATION_EXPORT double RWPickFlavor2VersionNumber;
+
+//! Project version string for RWPickFlavor2.
+FOUNDATION_EXPORT const unsigned char RWPickFlavor2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor2/PublicHeader.h>
+
+
